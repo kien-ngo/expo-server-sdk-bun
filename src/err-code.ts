caa4b8620460e7d6a673c0cb28bffc1fcbddb74d
Kien Ngo <github@kien.ngo>
2026-01-11 11:58:58 +0700

    Add types for err-code

--- src/err-code.ts
+++ src/err-code.ts
@@ -1,69 +1,53 @@
 // Source: https://github.com/IndigoUnited/js-err-code/blob/master/index.js
 
-/**
- * @typedef {{ [key: string]: any }} Extensions
- * @typedef {Error} Err
- * @property {string} message
- */
+type Extensions = Record<string, any>;
 
-/**
- *
- * @param {Error} obj
- * @param {Extensions} props
- * @returns {Error & Extensions}
- */
-function assign(obj, props) {
-    for (const key in props) {
-        Object.defineProperty(obj, key, {
-            value: props[key],
-            enumerable: true,
-            configurable: true,
-        });
-    }
+type Err = Error & Extensions;
 
-    return obj;
+function assign(obj: Error, props: Extensions): Error & Extensions {
+  for (const key in props) {
+    Object.defineProperty(obj, key, {
+      value: props[key],
+      enumerable: true,
+      configurable: true,
+    });
+  }
+
+  return obj;
 }
 
-/**
- *
- * @param {any} err - An Error
- * @param {string|Extensions} code - A string code or props to set on the error
- * @param {Extensions} [props] - Props to set on the error
- * @returns {Error & Extensions}
- */
-function createError(err, code, props) {
-    if (!err || typeof err === 'string') {
-        throw new TypeError('Please pass an Error to err-code');
-    }
+function createError(err: any, code: string | Extensions, props?: Extensions): Err {
+  if (!err || typeof err === 'string') {
+    throw new TypeError('Please pass an Error to err-code');
+  }
 
-    if (!props) {
-        props = {};
-    }
+  if (!props) {
+    props = {};
+  }
 
-    if (typeof code === 'object') {
-        props = code;
-        code = '';
-    }
+  if (typeof code === 'object') {
+    props = code as Extensions;
+    code = '';
+  }
 
-    if (code) {
-        props.code = code;
-    }
+  if (code) {
+    props['code'] = code;
+  }
 
-    try {
-        return assign(err, props);
-    } catch (_) {
-        props.message = err.message;
-        props.stack = err.stack;
+  try {
+    return assign(err, props);
+  } catch (_) {
+    props['message'] = err.message;
+    props['stack'] = err.stack;
 
-        const ErrClass = function () {};
+    const ErrClass: any = function () {};
 
-        ErrClass.prototype = Object.create(Object.getPrototypeOf(err));
+    ErrClass.prototype = Object.create(Object.getPrototypeOf(err));
 
-        // @ts-ignore
-        const output = assign(new ErrClass(), props);
+    const output = assign(new ErrClass(), props);
 
-        return output;
-    }
+    return output;
+  }
 }
 
 module.exports = createError;
