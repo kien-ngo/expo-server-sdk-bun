219889398f9cfed74856b216c0ab028028f790c2
Kien Ngo <github@kien.ngo>
2026-01-11 11:50:31 +0700

    Remove eslint and prettier in replacement for oxlint and oxfmt

--- eslint.config.ts
+++
@@ -1,4 +0,0 @@
-import { defineConfig, globalIgnores } from 'eslint/config';
-import universeNodeConfig from 'eslint-config-universe/flat/node';
-
-export default defineConfig([globalIgnores(['build', 'coverage']), universeNodeConfig]);
